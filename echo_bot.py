35540c7479d1e6b35cf436bbb04208c996a0ff92
cas2396 <59550205+cas2396@users.noreply.github.com>
2020-02-27 16:50:51 +0400

    первая версия

--- echo_bot.py
+++ echo_bot.py
@@ -3,21 +3,35 @@
 from _token import token
 import vk_api
 import vk_api.bot_longpoll
+import random
 
 group_id = 192423088
 
 
 class Bot:
 
-    def __init__(self, group_id, token):
-        self.group_id = group_id
-        self.token = token
-
-        self.vk = vk_api.VkApi(token=token)
+    def __init__(self, group_id_, token_):
+        self.group_id = group_id_
+        self.token = token_
+        self.vk = vk_api.VkApi(token=token_)
         self.long_poll = vk_api.bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
+        self.api = self.vk.get_api()
 
     def run(self):
-        pass
+        for event in self.long_poll.listen():
+            try:
+                self.on_event(event)
+            except Exception as exc:
+                print(exc)
+
+    def on_event(self, event):
+        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+            print(event.object.text)
+            self.api.messages.send(message=("Возвращаю строку: " + event.object.text),
+                                   random_id=random.randint(0, 2 ** 20),
+                                   peer_id=event.object.peer_id)
+        else:
+            print("не знаю событий такого типа", event.object.type)
 
 
 if __name__ == '__main__':
